dce9db98d64a020e7dc7c03c9ece3b47de733669
HzHaxx <hamberzq@gmail.com>
2024-06-06 15:09:06 -0500

    Añadiendo los atributos "name"

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -21,32 +21,32 @@
                 <input type="text" id="titulo" name="titulo" placeholder="Titulo Propiedad">
 
                 <label for="precio">Precio</label>
-                <input type="number" id="precio" placeholder="Precio Propiedad">
+                <input type="number" id="precio" name="precio" placeholder="Precio Propiedad">
 
                 <label for="imagen">Imagen</label>
                 <input type="file" id="imagen" accept="image/jpeg, image/png">
 
                 <label for="descripcion">Descripción</label>
-                <textarea id="descripcion"></textarea>
+                <textarea id="descripcion" name="descripcion"></textarea>
             </fieldset>
 
             <fieldset>
                 <legend>Información Propiedad</legend>
 
                 <label for="habitaciones">Habitaciones</label>
-                <input type="number" id="habitaciones" placeholder="Ej: 3" min="1" max="9">
+                <input type="number" id="habitaciones" name="habitaciones" placeholder="Ej: 3" min="1" max="9">
 
                 <label for="wc">Baños</label>
-                <input type="number" id="wc" placeholder="Ej: 2" min="1" max="9">
+                <input type="number" id="wc" name="wc" placeholder="Ej: 2" min="1" max="9">
 
                 <label for="estacionamiento">Estacionamiento</label>
-                <input type="number" id="estacionamiento" placeholder="Ej: 1" min="1" max="9">
+                <input type="number" id="estacionamiento" name = "estacionamiento" placeholder="Ej: 1" min="1" max="9">
             </fieldset>
 
             <fieldset>
                 <legend>Vendedor</legend>
 
-                <select>
+                <select name="vendedor">
                     <option value="1">Miguel</option>
                     <option value="2">Karen</option>
                 </select>
