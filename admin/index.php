ca9da0b32238be248fdbc45bc9ce5cbf1805a563
HzHaxx <hamberzq@gmail.com>
2024-06-13 10:50:29 -0500

    Tabla para listar Propiedades de la DB

--- admin/index.php
+++ admin/index.php
@@ -13,6 +13,31 @@
         <?php endif; ?>
 
         <a href="/bienesraices/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
+
+        <table class="propiedades">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Titulo</th>
+                    <th>Imagen</th>
+                    <th>Precio</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Casa en la playa</td>
+                    <td> <img src="" alt="" class="imagen-tabla"> </td>
+                    <td> $1200000</td>
+                    <td>
+                        <a href="#" class="boton-rojo-block">Eliminar</a>
+                        <a href="#" class="boton-amarillo-block">Actualizar</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </main>
 
 <?php
