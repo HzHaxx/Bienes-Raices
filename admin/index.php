501be90b2951bb143840669d66e73a0e2d3adc50
HzHaxx <hamberzq@gmail.com>
2024-06-13 12:05:41 -0500

    Haciendo la tabla dinámica con php

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,19 @@
 <?php
 
+    // Importa la conexión a la base de datos
+    require '../includes/config/database.php';
+    $db = conectarDB();
+
+    // Escribir el Query
+    $query = "SELECT * FROM propiedades";
+
+    // Consultar la base de datos
+    $resultadoConsulta = mysqli_query($db, $query);
+
+    // Muestra mensaje condicional
     $resultado = $_GET['mensaje'] ?? null;
+
+    // Incluye un tamplate
     require '../includes/funciones.php';
 
     incluirTemplate('header');
@@ -25,21 +38,26 @@
                 </tr>
             </thead>
 
-            <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>Casa en la playa</td>
-                    <td> <img src="" alt="" class="imagen-tabla"> </td>
-                    <td> $1200000</td>
-                    <td>
-                        <a href="#" class="boton-rojo-block">Eliminar</a>
-                        <a href="#" class="boton-amarillo-block">Actualizar</a>
-                    </td>
-                </tr>
+            <tbody> <!-- Mostrar los resultados -->
+                <?php while($propiedad = mysqli_fetch_assoc($resultadoConsulta)): ?>
+                    <tr>
+                        <td><?php echo $propiedad['id']; ?></td>
+                        <td><?php echo $propiedad['titulo']; ?></td>
+                        <td> <img src="/bienesraices/imagenes/<?php echo $propiedad['imagen']; ?>" alt="Imagen de la propiedad" class="imagen-tabla"> </td>
+                        <td> $<?php echo $propiedad['precio']; ?></td>
+                        <td>
+                            <a href="#" class="boton-rojo-block">Eliminar</a>
+                            <a href="#" class="boton-amarillo-block">Actualizar</a>
+                        </td>
+                    </tr>
+                <?php endwhile; ?>
             </tbody>
         </table>
     </main>
 
 <?php
+    // Cerrar la conexión
+    mysqli_close($db);
+
     incluirTemplate('footer');
 ?>
