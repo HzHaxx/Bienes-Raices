b6686aa3bb47209e0402594b32c11fec72bc5095
HzHaxx <hamberzq@gmail.com>
2024-06-19 10:11:00 -0500

    Añadimos la acción de Eliminar

--- admin/index.php
+++ admin/index.php
@@ -48,7 +48,12 @@
                         <td> <img src="/bienesraices/imagenes/<?php echo $propiedad['imagen']; ?>" alt="Imagen de la propiedad" class="imagen-tabla"> </td>
                         <td> $<?php echo $propiedad['precio']; ?></td>
                         <td>
-                            <a href="#" class="boton-rojo-block">Eliminar</a>
+                            <form method="POST" class="w-100">
+                                <input type="hidden" name="id" value="<?php echo $propiedad['id']; ?>">
+
+                                <input type="submit" class="boton-rojo-block" value="Eliminar">
+                            </form>
+
                             <a href="/bienesraices/admin/propiedades/actualizar.php?id=<?php echo $propiedad['id']; ?>" class="boton-amarillo-block">Actualizar</a>
                         </td>
                     </tr>
