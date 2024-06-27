756010002701d33533ba2850960c6a10fc9915e7
HzHaxx <hamberzq@gmail.com>
2024-06-27 10:19:35 -0500

    Hasheamos la contraseña

--- usuario.php
+++ usuario.php
@@ -8,6 +8,9 @@ $db = conectarDB();
 $email = "correo@correo.com";
 $password = "123456";
 
+// Hashear el password
+$passwordHash = password_hash($password, PASSWORD_DEFAULT);
+
 // Query para crear el usuario
 $query = "INSERT INTO usuarios (email, password) VALUES ('{$email}', '{$password}'); ";
 
