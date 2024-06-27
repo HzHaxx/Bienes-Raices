936682da4596bd7501c9cd5a2cfb8ca70a0d2bb5
HzHaxx <hamberzq@gmail.com>
2024-06-27 10:20:01 -0500

    Agregando la pagina de login

---
+++ login.php
@@ -0,0 +1,28 @@
+<?php
+    require 'includes/funciones.php';
+
+    incluirTemplate('header');
+?>
+
+    <main class="contenedor seccion contenido-centrado">
+        <h1>Iniciar Sesión</h1>
+
+        <form class="formulario" method="POST" action="">
+            <fieldset>
+                <legend>Email y Password</legend>
+
+                <label for="email">E-mail</label>
+                <input type="email" name="email" id="email" placeholder="Tu Correo Electrónico">
+
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" placeholder="Tu Password">
+
+            </fieldset>
+
+            <input type="submit" value="Iniciar Sesión" class="boton boton-verde">
+        </form>
+    </main>
+
+<?php
+    incluirTemplate('footer');
+?>
