8ba3b90901592457e5e8d9ce74650dadfdf8711b
HzHaxx <hamberzq@gmail.com>
2024-08-09 16:35:04 -0500

    Updating paths

--- anuncio.php
+++ anuncio.php
@@ -4,7 +4,7 @@
     $id = filter_var($id, FILTER_VALIDATE_INT);
 
     if (!$id) {
-        header('Location: /bienesraices/index.php');
+        header('Location: index.php');
     }
 
     // Importar la conexión
@@ -18,7 +18,7 @@
     $resultado = mysqli_query($db, $query);
 
     if (!$resultado->num_rows) {
-        header('Location: /bienesraices/index.php');
+        header('Location: index.php');
     }
 
     $propiedad = mysqli_fetch_assoc($resultado);
@@ -28,24 +28,24 @@
     incluirTemplate('header');
 ?>
 
+
     <main class="contenedor seccion contenido-centrado">
         <h1><?php echo $propiedad['titulo']; ?></h1>
-
-        <img loading="lazy" src="/bienesraices/imagenes/<?php echo $propiedad['imagen']; ?>" alt="Imagen de la propiedad">
+        <img loading="lazy" src="/imagenes/<?php echo $propiedad['imagen']; ?>" alt="Imagen de la propiedad">
 
         <div class="resumen-propiedad">
             <p class="precio">$<?php echo $propiedad['precio']; ?></p>
             <ul class="iconos-caracteristicas">
                 <li>
-                    <img class="icono" loading="lazy" src="build/img/icono_wc.svg" alt="Icono WC">
+                    <img class="icono" loading="lazy" src="/build/img/icono_wc.svg" alt="Icono WC">
                     <p><?php echo $propiedad['wc']; ?></p>
                 </li>
                 <li>
-                    <img class="icono" loading="lazy" src="build/img/icono_estacionamiento.svg" alt="Icono Estacionamiento">
+                    <img class="icono" loading="lazy" src="/build/img/icono_estacionamiento.svg" alt="Icono Estacionamiento">
                     <p><?php echo $propiedad['estacionamiento']; ?></p>
                 </li>
                 <li>
-                    <img class="icono" loading="lazy" src="build/img/icono_dormitorio.svg" alt="Icono Habitaciones">
+                    <img class="icono" loading="lazy" src="/build/img/icono_dormitorio.svg" alt="Icono Habitaciones">
                     <p><?php echo $propiedad['habitaciones']; ?></p>
                 </li>
             </ul>
