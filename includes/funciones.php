5a5082ab24626d0d4535ecea36fc9a223b450da4
HzHaxx <hamberzq@gmail.com>
2024-08-10 11:14:54 -0500

    Update funciones.php

--- includes/funciones.php
+++ includes/funciones.php
@@ -10,12 +10,16 @@ function incluirTemplate(string $nombre, bool $inicio = false) {
 function estaAutenticado(): bool {
     session_start();
 
-    $auth = $_SESSION['login'];
-
-    if ($auth) {
-        return true;
+    if (!$_SESSION['login']) {
+        header('Location: /');
     }
 
     return false;
 }
 
+function debuguear($variable) {
+    echo "<pre>";
+    var_dump($variable);
+    echo "</pre>";
+    exit;
+}
