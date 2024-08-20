129e01d8df78b89fbb51ef4d99f6c5db7f51b34b
HzHaxx <hamberzq@gmail.com>
2024-08-20 16:07:08 -0500

    Update ActiveRecord.php

--- classes/ActiveRecord.php
+++ classes/ActiveRecord.php
@@ -6,37 +6,13 @@ class ActiveRecord
 {
     // Base de datos
     protected static $db; // static para que no se duplique la conexión
-    protected static $columnasDB = ['id', 'titulo', 'precio', 'imagen', 'descripcion', 'habitaciones', 'wc', 'estacionamiento', 'creado', 'vendedorId'];
+    protected static $columnasDB = [];
     protected static $tabla = '';
 
     // Errores
     protected static $errores = [];
 
-    public $id;
-    public $titulo;
-    public $precio;
-    public $imagen;
-    public $descripcion;
-    public $habitaciones;
-    public $wc;
-    public $estacionamiento;
-    public $creado;
-    public $vendedorId;
-
-    public function __construct($args = [])
-    {
-        $this->id = $args['id'] ?? null;
-        $this->titulo = $args['titulo'] ?? '';
-        $this->precio = $args['precio'] ?? '';
-        $this->imagen = $args['imagen'] ?? '';
-        $this->descripcion = $args['descripcion'] ?? '';
-        $this->habitaciones = $args['habitaciones'] ?? '';
-        $this->wc = $args['wc'] ?? '';
-        $this->estacionamiento = $args['estacionamiento'] ?? '';
-        $this->creado = date('Y/m/d');
-        $this->vendedorId = $args['vendedorId'] ?? '';
-    }
-
+    
     public static function setDB($database)
     {
         self::$db = $database;
