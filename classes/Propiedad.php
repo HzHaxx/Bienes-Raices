77d7439c196e8e82b98b28360a5e741ce4885ee3
HzHaxx <hamberzq@gmail.com>
2024-08-21 12:52:20 -0500

    Update Propiedad.php

--- classes/Propiedad.php
+++ classes/Propiedad.php
@@ -31,4 +31,41 @@ class Propiedad extends ActiveRecord
         $this->creado = date('Y/m/d');
         $this->vendedorId = $args['vendedorId'] ?? null;
     }
+
+    public function validar()
+    {
+        if (!$this->titulo) {
+            self::$errores[] = "Debes añadir un título";
+        }
+
+        if (!$this->precio) {
+            self::$errores[] = "El precio es obligatorio";
+        }
+
+        if (strlen($this->descripcion) < 50) {
+            self::$errores[] = "La descripción es obligatoria y debe tener al menos 50 caracteres";
+        }
+
+        if (!$this->habitaciones) {
+            self::$errores[] = "El número de habitaciones es obligatorio";
+        }
+
+        if (!$this->wc) {
+            self::$errores[] = "El número de baños es obligatorio";
+        }
+
+        if (!$this->estacionamiento) {
+            self::$errores[] = "El número de estacionamientos es obligatorio";
+        }
+
+        if (!$this->vendedorId) {
+            self::$errores[] = "Elige un vendedor";
+        }
+
+        if (!$this->imagen) {
+            self::$errores[] = "La Imagen de la propiedad es obligatoria";
+        }
+
+        return self::$errores;
+    }
 }
