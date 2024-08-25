8e64a0346450e3dac56ae055228b795572d92c72
HzHaxx <hamberzq@gmail.com>
2024-08-24 20:52:48 -0500

    Función validar para vendedor

--- classes/Vendedor.php
+++ classes/Vendedor.php
@@ -19,4 +19,25 @@ class Vendedor extends ActiveRecord
         $this->apellido = $args['apellido'] ?? '';
         $this->telefono = $args['telefono'] ?? '';
     }
+
+    public function validar()
+    {
+        if (!$this->nombre) {
+            self::$errores[] = "Debes añadir un nombre";
+        }
+
+        if (!$this->apellido) {
+            self::$errores[] = "Debes añadir un apellido";
+        }
+
+        if (!$this->telefono) {
+            self::$errores[] = "Debes añadir un teléfono";
+        }
+
+        if (!preg_match('/[0-9]{9}/', $this->telefono)) {
+            self::$errores[] = "El teléfono solo puede contener números y debe tener 10 dígitos";
+        }
+
+        return self::$errores;
+    }
 }
