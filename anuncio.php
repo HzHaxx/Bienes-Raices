de602d168c346fa1d48b4ea408028f3a5e978264
HzHaxx <hamberzq@gmail.com>
2024-08-25 11:06:24 -0500

    Update anuncio.php

--- anuncio.php
+++ anuncio.php
@@ -27,33 +27,29 @@
 
 
     <main class="contenedor seccion contenido-centrado">
-        <h1><?php echo $propiedad['titulo']; ?></h1>
-        <img loading="lazy" src="/imagenes/<?php echo $propiedad['imagen']; ?>" alt="Imagen de la propiedad">
+        <h1><?php echo $propiedad->titulo; ?></h1>
+        <img loading="lazy" src="/imagenes/<?php echo $propiedad->imagen; ?>" alt="Imagen de la propiedad">
 
         <div class="resumen-propiedad">
-            <p class="precio">$<?php echo $propiedad['precio']; ?></p>
+            <p class="precio">$<?php echo $propiedad->precio; ?></p>
             <ul class="iconos-caracteristicas">
                 <li>
                     <img class="icono" loading="lazy" src="/build/img/icono_wc.svg" alt="Icono WC">
-                    <p><?php echo $propiedad['wc']; ?></p>
+                    <p><?php echo $propiedad->wc; ?></p>
                 </li>
                 <li>
                     <img class="icono" loading="lazy" src="/build/img/icono_estacionamiento.svg" alt="Icono Estacionamiento">
-                    <p><?php echo $propiedad['estacionamiento']; ?></p>
-                </li>
+                    <p><?php echo $propiedad->estacionamiento; ?></p>
                 <li>
                     <img class="icono" loading="lazy" src="/build/img/icono_dormitorio.svg" alt="Icono Habitaciones">
-                    <p><?php echo $propiedad['habitaciones']; ?></p>
+                    <p><?php echo $propiedad->habitaciones; ?></p>
                 </li>
             </ul>
 
-            <p><?php echo $propiedad['descripcion']; ?></p>
+            <p><?php echo $propiedad->descripcion; ?></p>
         </div>
     </main>
 
 <?php
-    // Cerrar la conexión
-    mysqli_close($db);
-
     incluirTemplate('footer');
 ?>
