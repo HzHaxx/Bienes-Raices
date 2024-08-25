3296ae07c07ffb882bc7a8e890af951ec87270da
HzHaxx <hamberzq@gmail.com>
2024-08-25 11:05:53 -0500

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -40,13 +40,12 @@
 
     <main class="contenedor">
         <h1>Administrador de Bienes Raices</h1>
-        <?php if(intval($resultado) === 1): ?>
-            <p class="alerta exito">Creado Correctamente</p> 
-        <?php elseif(intval($resultado) === 2): ?>
-            <p class="alerta exito">Actualizado Correctamente</p> 
-        <?php elseif(intval($resultado) === 3): ?>
-            <p class="alerta exito">Eliminado Correctamente</p>
-        <?php endif; ?>
+
+        <?php 
+            $mensaje = mostrarNotificacion(intval($resultado));
+            if ($mensaje) { ?>
+                <p class="alerta exito"><?php echo s($mensaje); ?></p>
+        <?php } ?>
 
         <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
         <a href="/admin/vendedores/crear.php" class="boton boton-amarillo">Nuevo Vendedor</a>
