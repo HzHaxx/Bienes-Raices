c1d29536514df3195168990f9e728988dc5da7c0
HzHaxx <hamberzq@gmail.com>
2024-09-01 20:41:27 -0500

    Update PropiedadController.php

--- controllers/PropiedadController.php
+++ controllers/PropiedadController.php
@@ -4,13 +4,16 @@ namespace Controllers;
 use MVC\Router;
 use Model\Propiedad;
 use Model\Vendedor;
+use Intervention\Image\ImageManagerStatic as Image;
 
 class PropiedadController
 {
     public static function index(Router $router)
     {
         $propiedades = Propiedad::all();
-        $resultado = null;
+        
+        // Mensaje condicional
+        $resultado = $_GET['resultado'] ?? null;
 
         $router->render('propiedades/admin', [
             'propiedades' => $propiedades,
@@ -23,24 +26,27 @@ class PropiedadController
         $propiedad = new Propiedad;
         $vendedores = Vendedor::all();
 
+        // Arreglo con mensajes de errores
+        $errores = Propiedad::getErrores();
+
+        // Ejecutar el código después de que el usuario envía el formulario
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $propiedad = new Propiedad($_POST['propiedad']);
 
             // Subida de archivos
             $imagen = $_FILES['propiedad']['tmp_name']['imagen'];
             $imagen = basename($_FILES['propiedad']['name']['imagen']);
             move_uploaded_file($imagen, __DIR__ . "/../public/imagenes/$imagen");
 
-            $resultado = $propiedad->save();
+                // Guardar en la base de datos
+                $propiedad->guardar();
 
-            if ($resultado) {
-                header('Location: /admin?resultado=' . $resultado);
             }
         }
 
         $router->render('propiedades/crear', [
             'propiedad' => $propiedad,
-            'vendedores' => $vendedores
+            'vendedores' => $vendedores,
+            'errores' => $errores
         ]);
     }
 
