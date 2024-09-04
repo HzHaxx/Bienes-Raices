8a0e3fc2b55cef0810ca8f1e1cde800f00132945
HzHaxx <hamberzq@gmail.com>
2024-09-03 20:43:28 -0500

    Update funciones.php

--- includes/funciones.php
+++ includes/funciones.php
@@ -58,3 +58,15 @@ function mostrarNotificacion($codigo) {
 
     return $mensaje;
 }
+
+// Validar URL
+function validarORedireccionar(string $url) {
+    $id = $_GET['id'];
+    $id = filter_var($id, FILTER_VALIDATE_INT);
+
+    if (!$id) {
+        header("Location: {$url}");
+    }
+
+    return $id;
+}
