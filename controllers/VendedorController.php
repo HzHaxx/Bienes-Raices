6d9a38ca0424018d12cb25a28bd5f697c05f4744
HzHaxx <hamberzq@gmail.com>
2024-09-05 10:48:50 -0500

    Create VendedorController.php

---
+++ controllers/VendedorController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Controllers;
+use MVC\Router;
+use Model\Vendedor;
+
+class VendedorController
+{
+    public static function index()
+    {
+        echo "Desde el controlador de vendedores";
+    }
+
+    public static function crear(Router $router)
+    {
+        $errores = Vendedor::getErrores();
+
+        $vendedor = new Vendedor;
+
+        $router->render('vendedores/crear', [
+            'errores' => $errores,
+            'vendedor' => $vendedor
+        ]);
+    }
+
+    public static function actualizar()
+    {
+        echo "Actualizar vendedor";
+    }
+
+    public static function eliminar()
+    {
+        echo "Eliminar vendedor";
+    }
+}
