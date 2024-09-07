04e060ff4afa81c84dde01d0e0f02ea762ccc134
HzHaxx <hamberzq@gmail.com>
2024-09-07 15:36:01 -0500

    Create PaginasController.php

---
+++ controllers/PaginasController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Controllers;
+
+class PaginasController
+{
+    public static function index()
+    {
+        echo "Desde el controlador de páginas";
+    }
+
+    public static function nosotros() {
+        echo "Desde Nosotros";
+    }
+
+    public static function propiedades() {
+        echo "Desde propiedades";
+    }
+
+    public static function propiedad() {
+        echo "Desde propiedad";
+    }
+
+    public static function blog() {
+        echo "Desde blog";
+    }
+
+    public static function entrada() {
+        echo "Desde entrada";
+    }
+
+    public static function contacto() {
+        echo "Desde contacto";
+    }
+}
