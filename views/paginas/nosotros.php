b98fd273c9d6fc6288202d2d0c204cea48e747b0
HzHaxx <hamberzq@gmail.com>
2024-09-08 13:55:04 -0500

    Create nosotros.php

---
+++ views/paginas/nosotros.php
@@ -0,0 +1,35 @@
+<main class="contenedor">
+    <h1>Conoce sobre Nosotros</h1>
+
+    <div class="contenido-nosotros">
+        <div class="imagen">
+            <picture>
+                <source srcset="build/img/nosotros.webp" type="image/webp">
+                <source srcset="build/img/nosotros.jpg" type="image/jpeg">
+                <img loading="lazy" src="build/img/nosotros.jpg" alt="Sobre Nosotros">
+            </picture>
+        </div>
+
+        <div class="texto-nosotros">
+            <blockquote>
+                25 Años de Experiencia
+            </blockquote>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque quod sunt animi. Excepturi tempora
+                officiis eaque ipsum. Cupiditate debitis veritatis vel, suscipit amet, impedit sequi laboriosam
+                ipsa, ut quod explicabo?. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi officiis
+                eum ratione non dignissimos assumenda ipsum facere error nulla quod laboriosam veritatis possimus
+                corrupti quam corporis deleniti velit, animi autem.</p>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, dolores quam? Rem error, esse corporis
+                minima nesciunt placeat ullam quis deserunt ad itaque maiores voluptatem corrupti, quam tempora
+                recusandae. Laboriosam?</p>
+        </div>
+    </div>
+</main>
+
+<section class="contenedor seccion">
+    <h1>Más Sobre Nosotros</h1>
+
+    <?php include 'iconos.php'; ?>
+</section>
