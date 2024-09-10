dd996622559486079a8e0d8380244242fe790222
HzHaxx <hamberzq@gmail.com>
2024-09-10 07:57:09 -0500

    Names innecesarios

--- views/paginas/contacto.php
+++ views/paginas/contacto.php
@@ -47,10 +47,10 @@
 
             <div class="forma-contacto">
                 <label for="contactar-telefono">Teléfono</label>
-                <input type="radio" name="contacto" id="contactar-telefono" value="telefono" name="contacto[contacto]" required>
+                <input type="radio" id="contactar-telefono" value="telefono" name="contacto[contacto]" required>
 
                 <label for="contactar-email">E-mail</label>
-                <input type="radio" name="contacto" id="contactar-email" value="email" name="contacto[contacto]" required>
+                <input type="radio" id="contactar-email" value="email" name="contacto[contacto]" required>
             </div>
 
             <p>Si eligió Teléfono, elija la fecha y la hora:</p>
