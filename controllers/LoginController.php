dae39bc7b25102af427765fb95574d67a8fa0dfb
HzHaxx <hamberzq@gmail.com>
2024-09-11 17:01:09 -0500

    Update LoginController.php

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -12,29 +12,22 @@ class LoginController
         $errores = [];
 
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $auth = $_POST;
-
-            // Validar que los campos no estén vacíos
-            if (!$auth['email']) {
-                $errores[] = 'El email es obligatorio';
-            }
-
-            if (!$auth['password']) {
-                $errores[] = 'El password es obligatorio';
-            }
+            $auth = new Admin($_POST);
+            $errores = $auth->validar();
 
             if (empty($errores)) {
-                // Revisar si el usuario existe
-                $usuario = Admin::where('email', $auth['email'])->first();
+                // Verificar si el usuario existe
+                $resultado = $auth->existeUsuario();
 
-                if ($usuario) {
-                    // Revisar si el password es correcto
-                    if (password_verify($auth['password'], $usuario->password)) {
+                if (!$resultado) {
+                    $errores = Admin::getErrores();
+                } else {
+                    // Verificar el password
+                    $autenticado = $auth->comprobarPassword($resultado);
+
+                    if ($autenticado) {
                         // Autenticar el usuario
-                        session_start();
-
-                        $_SESSION['usuario'] = $usuario->email;
-                        $_SESSION['login'] = true;
+                        $auth->autenticar();
 
                         header('Location: /admin');
                     } else {
