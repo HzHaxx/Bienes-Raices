daa18e0e16a4904832c8fae16a716a154a489fe9
HzHaxx <hamberzq@gmail.com>
2024-09-10 20:30:28 -0500

    Create login.php

---
+++ views/Auth/login.php
@@ -0,0 +1,24 @@
+<main class="contenedor seccion contenido-centrado">
+    <h1>Iniciar Sesión</h1>
+
+    <?php foreach ($errores as $error): ?>
+        <div class="alerta error">
+            <?php echo $error; ?>
+        </div>
+    <?php endforeach; ?>
+
+    <form class="formulario" method="POST" action="/login">
+        <fieldset>
+            <legend>Email y Password</legend>
+
+            <label for="email">E-mail</label>
+            <input type="email" name="email" id="email" placeholder="Tu Correo Electrónico">
+
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password" placeholder="Tu Password">
+
+        </fieldset>
+
+        <input type="submit" value="Iniciar Sesión" class="boton boton-verde">
+    </form>
+</main>
