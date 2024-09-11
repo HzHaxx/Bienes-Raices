e98419ad7abf37c7af07f7cf011471183f553d79
HzHaxx <hamberzq@gmail.com>
2024-09-11 17:01:24 -0500

    Update Admin.php

--- models/Admin.php
+++ models/Admin.php
@@ -19,11 +19,54 @@ class Admin extends ActiveRecord
         $this->password = $args['password'] ?? '';
     }
 
-    public static function login($datos)
+    public function validar()
     {
-        if ($datos['usuario'] === 'admin' && $datos['password'] === 'admin') {
-            return true;
+        if (!$this->email) {
+            self::$errores[] = "El email es obligatorio";
         }
-        return false;
+
+        if (!$this->password) {
+            self::$errores[] = "El password es obligatorio";
+        }
+
+        return self::$errores;
+    }
+
+    public function existeUsuario()
+    {
+        // Revisar si un usuario existe o no
+        $query = "SELECT * FROM " . self::$tabla . " WHERE email = '" . $this->email . "' LIMIT 1";
+        $resultado = self::$db->query($query);
+
+        if (!$resultado->num_rows) {
+            self::$errores[] = "El usuario no existe";
+            return;
+        }
+
+        return $resultado;
+    }
+
+    public function comprobarPassword($resultado)
+    {
+        $usuario = $resultado->fetch_object();
+
+        $autenticado = password_verify($this->password, $usuario->password);
+
+        if (!$autenticado) {
+            self::$errores[] = "El password es incorrecto";
+            return;
+        }
+
+        return $autenticado;
+    }
+
+    public function autenticar() {
+        session_start();
+
+        // Llenar el arreglo de la sesión
+        $_SESSION['usuario'] = $this->email;
+        $_SESSION['login'] = true;
+
+        header('Location: /admin');
     }
 }
